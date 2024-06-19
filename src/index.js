edaac947eb33b2c8d0ed16d6d7cb8da5503d8583
Arthur Frade de Araujo <arthur4raujo@gmail.com>
2024-06-19 11:57:32 -0300

    gitignore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 // server.mjs
 const { createServer } = require('node:http');
+const { NamesTable } = require('./name_table');
+const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator');
 
-const server = createServer((req, res) => {
+const server = createServer(async (req, res) => {
+  const name = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals] });
+  await NamesTable.create({ name });
+
+  const names = await NamesTable.findAll();
+
+  const response = '<h1>Full Cycle Rocks!</h1>\n';
+  response += '<ul>\n';
+  response += names.map(nameRegister => `<li>${nameRegister.name}</li>`).join('\n');
+  response += '</ul>\n';
+
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Full Cycle Rocks!</h1>\n');
+  res.end(response);
 });
 
 // starts a simple http server locally on port 3000
